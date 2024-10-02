6bb2c3981533db878b2bb608127e07c2e5fdbbac
Spencer Wang <ziruiwang.spencer@gmail.com>
2024-10-02 13:11:01 -0400

    testing

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ import { getTotalPoints, order, getEvent } from './functions/pointCalc.js';
 var data = [['Activity A', '100', '20', '30', '40'], ['Activity B', '50', '60', '70', '80'], ['Activity C', '90', '100', '110', '120']];
 // console.log('Fetched Data: \n', data);
 
-let ev = getEvent(data[0]);
+var ev = getEvent(data[0]);
 console.log('Winner:', ev.getWinner());
 console.log('Scores:', ev.getScore());
